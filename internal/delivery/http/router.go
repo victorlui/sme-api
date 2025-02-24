e80bda057a5519567ea4096d4aeab44e213ea691
Victor Luiz <victorluizgonzalez@gmail.com>
2025-02-24 14:21:13 -0300

    Configurando o CORS da aplicação

--- internal/delivery/http/router.go
+++ internal/delivery/http/router.go
@@ -15,7 +15,7 @@ func SetupRouter(db *db.PostgresDB) *gin.Engine {
 	r := gin.New()
 
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000", "http://127.0.0.1:3000"}, // Ou especifique os domínios permitidos
+		AllowOrigins:     []string{"http://localhost:3000", "http://127.0.0.1:3000", "*"}, // Ou especifique os domínios permitidos
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
